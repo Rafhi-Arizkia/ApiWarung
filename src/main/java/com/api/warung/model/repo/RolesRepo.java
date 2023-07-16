df16db04f9dfbbf4baeb273ab274694de4a19a76
Dandelions16 <doyokbsk@gmail.com>
2023-07-16 22:08:55 +0700

    build:

--- src/main/java/com/api/warung/model/repo/RolesRepo.java
+++ src/main/java/com/api/warung/model/repo/RolesRepo.java
@@ -1,2 +1,10 @@
-package com.api.warung.model.repo;public class RolesRepo {
+package com.api.warung.model.repo;
+
+import com.api.warung.model.entitiy.RolesEntities;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RolesRepo extends JpaRepository<RolesEntities,Long> {
+    RolesEntities findByRolesName(String rolesName);
 }
