da432272d31aa351385e4572ba5cefe86b531219
Dandelions16 <doyokbsk@gmail.com>
2023-07-16 22:09:11 +0700

    build:

--- src/main/java/com/api/warung/model/repo/UserRepo.java
+++ src/main/java/com/api/warung/model/repo/UserRepo.java
@@ -1,2 +1,12 @@
-package com.api.warung.model.repo;public interface UserRepo {
+package com.api.warung.model.repo;
+
+import com.api.warung.model.entitiy.UserEntities;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepo extends JpaRepository<UserEntities,Long> {
+    Optional<UserEntities> findByUserEmail(String userEmail);
 }
