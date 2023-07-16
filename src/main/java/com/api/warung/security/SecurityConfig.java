ac9669eb750878b0591bffbd35e5a540311f7c73
Dandelions16 <doyokbsk@gmail.com>
2023-07-16 22:09:28 +0700

    build:

--- src/main/java/com/api/warung/security/SecurityConfig.java
+++ src/main/java/com/api/warung/security/SecurityConfig.java
@@ -1,2 +1,28 @@
-package com.api.warung.security;public class SecurityConfig {
+package com.api.warung.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/warung/**","/actuator/**")
+                        .permitAll())
+                .build();
+    }
 }
