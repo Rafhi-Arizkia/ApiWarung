1aad01a7dd774454a3c452cd456eafaf0f7a5192
Dandelions16 <doyokbsk@gmail.com>
2023-07-17 15:37:19 +0700

    build:

---
+++ src/main/java/com/api/warung/dto/AuthToken.java
@@ -0,0 +1,14 @@
+package com.api.warung.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+public class AuthToken {
+    private String token;
+}
