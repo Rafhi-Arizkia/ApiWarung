b3962805f848c2334c6cab633f787e2643db2418
Dandelions16 <doyokbsk@gmail.com>
2023-07-19 12:33:30 +0700

    build:

--- src/main/java/com/api/warung/model/entitiy/MenuEntities.java
+++ src/main/java/com/api/warung/model/entitiy/MenuEntities.java
@@ -14,6 +14,8 @@ public class MenuEntities {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id_menu;
     private byte[] image;
+    @Column(name = "nama_menu",length = 50,nullable = false)
+    private String nameMenu;
     @Column(name = "description_menu",length = 150,nullable = false)
     private String descriptionMenu;
     private Double priceMenu;
