450e2e79ceb9d3c3e3ae97cba53818fd78705d8c
Dandelions16 <doyokbsk@gmail.com>
2023-07-19 12:33:10 +0700

    build:

---
+++ src/main/java/com/api/warung/model/repo/CategoryRepo.java
@@ -0,0 +1,9 @@
+package com.api.warung.model.repo;
+
+import com.api.warung.model.entitiy.CategoryEntities;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepo extends JpaRepository<CategoryEntities,Long> {
+}
