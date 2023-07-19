1d9fd433c2a738430cf75d10cf50d7c8fc959d3b
Dandelions16 <doyokbsk@gmail.com>
2023-07-19 12:32:47 +0700

    build:

--- src/main/java/com/api/warung/model/entitiy/CategoryEntities.java
+++ src/main/java/com/api/warung/model/entitiy/CategoryEntities.java
@@ -13,10 +13,6 @@ public class CategoryEntities {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id_category;
-    @Column(name = "category_makanan", length = 50, nullable = false)
-    private String categoryMakanan;
-    @Column(name = "cataegory_minuman", length =50, nullable = false)
-    private String categoryMinuman;
-    @Column(name = "category_snack", length = 50, nullable = false)
-    private String categorySnack;
+    @Column(name = "category_name", length = 50, nullable = false)
+    private String categoryName;
 }
