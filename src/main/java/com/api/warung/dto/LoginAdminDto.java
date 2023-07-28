1b72c44c67bf9d054241633c155d0919fe444e49
Dandelions16 <doyokbsk@gmail.com>
2023-07-28 15:37:57 +0700

    feature(LoginAdminDto): membuat dto untuk admin login

---
+++ src/main/java/com/api/warung/dto/LoginAdminDto.java
@@ -0,0 +1,16 @@
+package com.api.warung.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginAdminDto {
+    @Email
+    @NotEmpty(message = "Required email for login")
+    private String email;
+    @NotEmpty(message = "Required password for login")
+    private String password;
+}
