81e19d70673e3f9f04b0c9f04abfdb4b88615198
Dandelions16 <doyokbsk@gmail.com>
2023-07-28 15:38:24 +0700

    feature(LoginAdminDto): membuat dto untuk admin register

---
+++ src/main/java/com/api/warung/dto/RegisterAdminDto.java
@@ -0,0 +1,24 @@
+package com.api.warung.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterAdminDto {
+    @NotEmpty(message = "Required Username")
+    private String userName;
+    @Email
+    @NotEmpty(message = "Required Email")
+    private String userEmail;
+    @NotEmpty(message = "Required Address")
+    private String userAddress;
+    @NotNull(message = "Required Number Phone")
+    private Long numberPhone;
+    @NotEmpty(message = "Required password")
+    private String password;
+
+}
